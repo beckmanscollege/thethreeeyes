8f7c3ed645c327bb2a22d8f77f83cd1a0a5d437a
Glitch (spotted-buttercup-mechanic) <none>
2023-02-11 15:11:22 +0000

    💘🙌 Checkpoint ./script.js:53671909/933 ./page2.html:53671909/2 ./page3.html:53671909/2209 ./style.css:53671909/7739

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const submit = document.getElementById('submit');
 let answer = document.getElementById('answer');
-const target = "cooper cooper cooper";
+const target = "cooper/cooper/cooper";
 
 
 answer.addEventListener('input', function(event) {
@@ -17,7 +17,24 @@ submit.addEventListener('click', function(event) {
   
 });
 
- 
+const submit2 = document.getElementById('submit2');
+let answer2 = document.getElementById('answer2');
+const target2 = "home";
+
+
+answer2.addEventListener('input', function(event) {
+  
+ if(this.value == target2) {
+    submit2.disabled = false;
+  }
+  else {
+    submit2.disabled = true; 
+  }
+});
+
+submit2.addEventListener('click', function(event) {
+  
+});
 
  
 
