a1bc63999685c6b0aca24d9300f3d13bd1e2a0c2
Glitch (spotted-buttercup-mechanic) <none>
2023-02-12 13:01:25 +0000

    📻🧀 Checkpoint ./here.am.i.css:53671909/53 ./script2.js:53671909/294

--- script2.js
+++ script2.js
@@ -1,11 +1,27 @@
 const submit = document.getElementById('submit2');
 let answer = document.getElementById('answer2');
-const target = "home";
+let target = "home";
+let target2 = "earth";
+
 
 
 answer.addEventListener('input', function(event) {
   
  if(this.value == target) {
+    submit.disabled = false;
+  }
+  else {
+    submit.disabled = true; 
+  }
+});
+
+submit.addEventListener('click', function(event) {
+  
+});
+
+answer.addEventListener('input', function(event) {
+  
+ if(this.value == target2) {
     submit.disabled = false;
   }
   else {
